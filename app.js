6b9b3267cb6fb95d51d3c29ba5512ccfeec4ea7d
Richiez6 <Richardxau6@gmail.com>
2026-08-04 20:09:53 +0100

    Show deployment slot

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Home Page
+const deploymentSlot = process.env.DEPLOYMENT_SLOT || "Unknown";
 app.get("/", (req, res) => {
     res.send(`
         <h1>QuickTill Checkout Service</h1>
         <h2>Version 1.0</h2>
         <p>Status: Running</p>
-        <p>Deployment Slot: Blue</p>
+        <p>Deployment Slot: ${deploymentSlot}</p>
     `);
 });
 
